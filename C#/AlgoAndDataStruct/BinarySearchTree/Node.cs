d388d9e29f709e1ed1d3fc1183e644bfc23859a7
Yong Wen Chua <yongwen@lshift.net>
2015-04-24 11:44:48 +0100

    Add BST

---
+++ C#/AlgoAndDataStruct/BinarySearchTree/Node.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace BinarySearchTree
+{
+    public class Node<T> where T : IComparable
+    {
+        public Node<T> Parent { get; set; }
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+
+        public T Value { get; set; }
+
+        public static Node<T> MakeNode(T value, Node<T> parent = null, Node<T> left = null, Node<T> right = null)
+        {
+            return new Node<T>() {Value = value, LeftChild = left, RightChild = right, Parent = parent};
+        }
+
+        public Node<T> Insert(T value)
+        {
+            var node = MakeNode(value);
+            Insert(node);
+            return node;
+        }
+
+        public void Insert(Node<T> node)
+        {
+            if (node == null) return;
+            // Node needs to be inserted into a tree
+            node.Parent = null;
+
+            var currentNode = this;
+            while (node.Parent == null)
+            {
+                var expression = node.Value.CompareTo(currentNode.Value) <= 0
+                    ? LeftChildExpression
+                    : RightChildExpression;
+                var newNode = expression.Compile()(currentNode);
+                // Bingo!
+                if (newNode == null)
+                {
+                    var selector = expression.Body as MemberExpression;
+                    if (selector != null)
+                    {
+                        var property = selector.Member as PropertyInfo;
+                        if (property != null)
+                        {
+                            property.SetValue(currentNode, node, null);
+                        }
+                    }
+                    node.Parent = currentNode;
+                }
+                else
+                {
+                    currentNode = newNode;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public static Expression<Func<Node<T>, Node<T>>> LeftChildExpression = n => n.LeftChild;
+        public static Expression<Func<Node<T>, Node<T>>> RightChildExpression = n => n.RightChild;
+
+        public static Node<T> MakeTree(params T[] values)
+        {
+            return MakeTree(values.AsEnumerable());
+        } 
+
+        public static Node<T> MakeTree(IEnumerable<T> values)
+        {
+            Node<T> root = null;
+
+            foreach (var value in values)
+            {
+                // Root Node
+                if (root == null)
+                {
+                    root = MakeNode(value);
+                }
+                else
+                {
+                    root.Insert(value);
+                }
+            }
+
+            return root;
+        }
+    }
+}
---
+++ C#/AlgoAndDataStruct/BinarySearchTree/Tests/NodeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BinarySearchTree.Tests
+{
+    [TestFixture]
+    public class NodeTests
+    {
+        private readonly int[] values = { 15, 6, 3, 2, 4, 7, 13, 9, 18, 17, 20};
+
+        [Test]
+        public void InsertionRespectsBinaryTreeProperty()
+        {
+            var root = Node<int>.MakeTree(values);
+            Assert.That(BinaryTreePropertyRespected(root));
+        }
+
+        public static bool BinaryTreePropertyRespected<T>(Node<T> root) where T : IComparable
+        {
+            if (root == null) return true;
+            var result = true;
+            if (root.LeftChild != null)
+            {
+                result &= root.LeftChild.Value.CompareTo(root.Value) <= 0;
+                result &= BinaryTreePropertyRespected<T>(root.LeftChild);
+            }
+
+            if (root.RightChild != null)
+            {
+                result &= root.RightChild.Value.CompareTo(root.Value) >= 0;
+                result &= BinaryTreePropertyRespected<T>(root.RightChild);
+            }
+
+            return result;
+        }
+    }
+}
